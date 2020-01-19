913f11e62230520a0f2e41e3472e7ac7719c2058
pphongsopa <pphongsopa@wpi.edu>
2020-01-18 20:03:53 -0500

    DATABASE

--- app/src/main/java/dramaticAccident/got2go/DatabaseHelper.java
+++ app/src/main/java/dramaticAccident/got2go/DatabaseHelper.java
@@ -1,5 +1,6 @@
 package dramaticAccident.got2go;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -12,31 +13,33 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     public static final String ToiletDB = "Toilet.db";
     public static final String ToiletTB = "Toilet_table";
-    public static final String ToiletID = "ToiletID";
+    public static final String ToiletID = "TOILETID";
     public static final String ToiletName = "NAME";
-    public static final String TableCoor = "COORDINATE";
-
-    public static final String RatingDB = "Rating.db";
-    public static final String RatingTB = "Rating_table";
-    public static final String RatingID = "RATINGID";
-    public static final String Comment = "COMMENT";
-    public static final String Rating = "RATING";
+    public static final String ToiletCoor = "COORDINATE";
 
 
     public DatabaseHelper(Context context) {
         super(context, ToiletDB, null, 1);
 
-        SQLiteDatabase db = this.getWritableDatabase();
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + ToiletTB +" (ToiletID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, COORDINATES TEXT)");
+        db.execSQL("create table " + ToiletTB +" (ToiletID INTEGER PRIMARY KEY AUTOINCREMENT, ToiletName TEXT, ToiletCoor TEXT)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS "+ToiletDB);
         onCreate(db);
+    }
+
+    public boolean addToilet(String name, String coor){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentvalues = new ContentValues();
+        contentvalues.put(ToiletName, name);
+        contentvalues.put(ToiletCoor, coor);
+        long result = db.insert(ToiletTB, null, contentvalues);
+        return !(result == -1);
     }
 }
